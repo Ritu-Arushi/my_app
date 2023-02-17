fdfa02bcc30003cec895d356d34e02f8f34f3c02
ritu kumari <arushiritu4@gmail.com>
2023-02-17 21:43:33 +0530

    first commit

---
+++ atri_app/app/src/page-cbs/Home.js
@@ -0,0 +1,250 @@
+import { useCallback } from "react";
+import { callbackFactory } from "../utils/callbackFactory";
+export function useFlex2Cb() {
+	const onClick = useCallback(callbackFactory("Flex2", "Home", "/Home", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useDiv2Cb() {
+	const onClick = useCallback(callbackFactory("Div2", "Home", "/Home", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useImage7Cb() {
+	const onClick = useCallback(callbackFactory("Image7", "Home", "/Home", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useLink18Cb() {
+
+	return {  }
+}
+export function useLink19Cb() {
+
+	return {  }
+}
+export function useLink20Cb() {
+
+	return {  }
+}
+export function useLink21Cb() {
+
+	return {  }
+}
+export function useLink22Cb() {
+
+	return {  }
+}
+export function useFlex3Cb() {
+	const onClick = useCallback(callbackFactory("Flex3", "Home", "/Home", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useButton8Cb() {
+	const onClick = useCallback(callbackFactory("Button8", "Home", "/Home", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useButton9Cb() {
+	const onClick = useCallback(callbackFactory("Button9", "Home", "/Home", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useFlex4Cb() {
+	const onClick = useCallback(callbackFactory("Flex4", "Home", "/Home", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useDiv5Cb() {
+	const onClick = useCallback(callbackFactory("Div5", "Home", "/Home", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useDiv6Cb() {
+	const onClick = useCallback(callbackFactory("Div6", "Home", "/Home", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useTextBox2Cb() {
+	const onClick = useCallback(callbackFactory("TextBox2", "Home", "/Home", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useTextBox3Cb() {
+	const onClick = useCallback(callbackFactory("TextBox3", "Home", "/Home", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useTextBox4Cb() {
+	const onClick = useCallback(callbackFactory("TextBox4", "Home", "/Home", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useFlex5Cb() {
+	const onClick = useCallback(callbackFactory("Flex5", "Home", "/Home", "onClick", 
+			{
+  "handlers": [],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useButton10Cb() {
+	const onClick = useCallback(callbackFactory("Button10", "Home", "/Home", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useButton11Cb() {
+	const onClick = useCallback(callbackFactory("Button11", "Home", "/Home", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
+export function useImage8Cb() {
+	const onClick = useCallback(callbackFactory("Image8", "Home", "/Home", "onClick", 
+			{
+  "handlers": [
+    {
+      "sendEventData": true
+    }
+  ],
+  "actions": [
+    {
+      "type": "do_nothing"
+    }
+  ]
+}), [])
+	return { onClick }
+}
---
+++ atri_app/app/src/pages/Home.jsx
@@ -0,0 +1,127 @@
+import { useLayoutEffect, useEffect } from "react";
+import useStore, { updateStoreStateFromController } from "../hooks/useStore";
+import useIoStore from "../hooks/useIoStore";
+import { useNavigate, useLocation } from "react-router-dom";
+import { subscribeInternalNavigation } from "../utils/navigate";
+import {fetchPageProps} from "../utils/fetchPageProps"
+import { Flex } from "@atrilabs/react-component-manifests/src/manifests/Flex/Flex.tsx";
+import { Div } from "@atrilabs/react-component-manifests/src/manifests/Div/Div.tsx";
+import { Image } from "@atrilabs/react-component-manifests/src/manifests/Image/Image.tsx";
+import { Link } from "@atrilabs/react-component-manifests/src/manifests/Link/Link.tsx";
+import { Button } from "@atrilabs/react-component-manifests/src/manifests/Button/Button.tsx";
+import { TextBox } from "@atrilabs/react-component-manifests/src/manifests/TextBox/TextBox.tsx";
+import { useFlex2Cb, useDiv2Cb, useFlex3Cb, useFlex4Cb, useDiv5Cb, useFlex5Cb, useDiv6Cb, useImage7Cb, useLink18Cb, useLink19Cb, useLink20Cb, useLink21Cb, useLink22Cb, useButton8Cb, useButton9Cb, useTextBox2Cb, useTextBox3Cb, useTextBox4Cb, useButton10Cb, useButton11Cb, useImage8Cb } from "../page-cbs/Home";
+import "../page-css/Home.css";
+import "../custom/Home";
+
+export default function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsub = subscribeInternalNavigation((url) => {
+      navigate(url);
+    });
+    return unsub;
+  }, [navigate]);
+
+  const location = useLocation();
+  useLayoutEffect(()=>{
+    fetchPageProps(location.pathname, location.search).then((res)=>{
+      updateStoreStateFromController(res.pageName, res.pageState)
+    })
+  }, [location])
+
+  const Flex2Props = useStore((state)=>state["Home"]["Flex2"]);
+const Flex2IoProps = useIoStore((state)=>state["Home"]["Flex2"]);
+const Flex2Cb = useFlex2Cb()
+const Div2Props = useStore((state)=>state["Home"]["Div2"]);
+const Div2IoProps = useIoStore((state)=>state["Home"]["Div2"]);
+const Div2Cb = useDiv2Cb()
+const Flex3Props = useStore((state)=>state["Home"]["Flex3"]);
+const Flex3IoProps = useIoStore((state)=>state["Home"]["Flex3"]);
+const Flex3Cb = useFlex3Cb()
+const Flex4Props = useStore((state)=>state["Home"]["Flex4"]);
+const Flex4IoProps = useIoStore((state)=>state["Home"]["Flex4"]);
+const Flex4Cb = useFlex4Cb()
+const Div5Props = useStore((state)=>state["Home"]["Div5"]);
+const Div5IoProps = useIoStore((state)=>state["Home"]["Div5"]);
+const Div5Cb = useDiv5Cb()
+const Flex5Props = useStore((state)=>state["Home"]["Flex5"]);
+const Flex5IoProps = useIoStore((state)=>state["Home"]["Flex5"]);
+const Flex5Cb = useFlex5Cb()
+const Div6Props = useStore((state)=>state["Home"]["Div6"]);
+const Div6IoProps = useIoStore((state)=>state["Home"]["Div6"]);
+const Div6Cb = useDiv6Cb()
+const Image7Props = useStore((state)=>state["Home"]["Image7"]);
+const Image7IoProps = useIoStore((state)=>state["Home"]["Image7"]);
+const Image7Cb = useImage7Cb()
+const Link18Props = useStore((state)=>state["Home"]["Link18"]);
+const Link18IoProps = useIoStore((state)=>state["Home"]["Link18"]);
+const Link18Cb = useLink18Cb()
+const Link19Props = useStore((state)=>state["Home"]["Link19"]);
+const Link19IoProps = useIoStore((state)=>state["Home"]["Link19"]);
+const Link19Cb = useLink19Cb()
+const Link20Props = useStore((state)=>state["Home"]["Link20"]);
+const Link20IoProps = useIoStore((state)=>state["Home"]["Link20"]);
+const Link20Cb = useLink20Cb()
+const Link21Props = useStore((state)=>state["Home"]["Link21"]);
+const Link21IoProps = useIoStore((state)=>state["Home"]["Link21"]);
+const Link21Cb = useLink21Cb()
+const Link22Props = useStore((state)=>state["Home"]["Link22"]);
+const Link22IoProps = useIoStore((state)=>state["Home"]["Link22"]);
+const Link22Cb = useLink22Cb()
+const Button8Props = useStore((state)=>state["Home"]["Button8"]);
+const Button8IoProps = useIoStore((state)=>state["Home"]["Button8"]);
+const Button8Cb = useButton8Cb()
+const Button9Props = useStore((state)=>state["Home"]["Button9"]);
+const Button9IoProps = useIoStore((state)=>state["Home"]["Button9"]);
+const Button9Cb = useButton9Cb()
+const TextBox2Props = useStore((state)=>state["Home"]["TextBox2"]);
+const TextBox2IoProps = useIoStore((state)=>state["Home"]["TextBox2"]);
+const TextBox2Cb = useTextBox2Cb()
+const TextBox3Props = useStore((state)=>state["Home"]["TextBox3"]);
+const TextBox3IoProps = useIoStore((state)=>state["Home"]["TextBox3"]);
+const TextBox3Cb = useTextBox3Cb()
+const TextBox4Props = useStore((state)=>state["Home"]["TextBox4"]);
+const TextBox4IoProps = useIoStore((state)=>state["Home"]["TextBox4"]);
+const TextBox4Cb = useTextBox4Cb()
+const Button10Props = useStore((state)=>state["Home"]["Button10"]);
+const Button10IoProps = useIoStore((state)=>state["Home"]["Button10"]);
+const Button10Cb = useButton10Cb()
+const Button11Props = useStore((state)=>state["Home"]["Button11"]);
+const Button11IoProps = useIoStore((state)=>state["Home"]["Button11"]);
+const Button11Cb = useButton11Cb()
+const Image8Props = useStore((state)=>state["Home"]["Image8"]);
+const Image8IoProps = useIoStore((state)=>state["Home"]["Image8"]);
+const Image8Cb = useImage8Cb()
+
+  return (<>
+  <Flex className="p-Home Flex2 bpt" {...Flex2Props} {...Flex2Cb} {...Flex2IoProps}>
+<Image className="p-Home Image7 bpt" {...Image7Props} {...Image7Cb} {...Image7IoProps}/>
+<Link className="p-Home Link22 bpt" {...Link22Props} {...Link22Cb} {...Link22IoProps}/>
+<Link className="p-Home Link21 bpt" {...Link21Props} {...Link21Cb} {...Link21IoProps}/>
+<Link className="p-Home Link20 bpt" {...Link20Props} {...Link20Cb} {...Link20IoProps}/>
+<Link className="p-Home Link19 bpt" {...Link19Props} {...Link19Cb} {...Link19IoProps}/>
+<Link className="p-Home Link18 bpt" {...Link18Props} {...Link18Cb} {...Link18IoProps}/>
+<Div className="p-Home Div2 bpt" {...Div2Props} {...Div2Cb} {...Div2IoProps}>
+<Flex className="p-Home Flex3 bpt" {...Flex3Props} {...Flex3Cb} {...Flex3IoProps}>
+<Button className="p-Home Button8 bpt" {...Button8Props} {...Button8Cb} {...Button8IoProps}/>
+<Button className="p-Home Button9 bpt" {...Button9Props} {...Button9Cb} {...Button9IoProps}/>
+</Flex>
+</Div>
+</Flex>
+<Flex className="p-Home Flex4 bpt" {...Flex4Props} {...Flex4Cb} {...Flex4IoProps}>
+<Div className="p-Home Div5 bpt" {...Div5Props} {...Div5Cb} {...Div5IoProps}>
+<TextBox className="p-Home TextBox2 bpt" {...TextBox2Props} {...TextBox2Cb} {...TextBox2IoProps}/>
+<TextBox className="p-Home TextBox3 bpt" {...TextBox3Props} {...TextBox3Cb} {...TextBox3IoProps}/>
+<TextBox className="p-Home TextBox4 bpt" {...TextBox4Props} {...TextBox4Cb} {...TextBox4IoProps}/>
+<Flex className="p-Home Flex5 bpt" {...Flex5Props} {...Flex5Cb} {...Flex5IoProps}>
+<Button className="p-Home Button11 bpt" {...Button11Props} {...Button11Cb} {...Button11IoProps}/>
+<Button className="p-Home Button10 bpt" {...Button10Props} {...Button10Cb} {...Button10IoProps}/>
+</Flex>
+</Div>
+<Div className="p-Home Div6 bpt" {...Div6Props} {...Div6Cb} {...Div6IoProps}>
+<Image className="p-Home Image8 bpt" {...Image8Props} {...Image8Cb} {...Image8IoProps}/>
+</Div>
+</Flex>
+  </>);
+}
